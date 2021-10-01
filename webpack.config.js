72b1509b6f61e12fdc509d1a1d43209ed7274956
Antonio <antonio.aguilar.r95@gmail.com>
2021-10-01 13:17:06 +1000

    Webpack Config

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,13 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   //This property defines where the application starts
   entry:'./src/index.js',
-    
+  watch: true,
   //This property defines the file path and the file name which will be used for deploying the bundled file
   output:{
-    path: path.join(__dirname, '/dist'),
+    path: path.resolve(__dirname, '/dist'),
     filename: 'bundle.js'
   },
     
@@ -18,8 +19,33 @@ module.exports = {
         test: /\.js$/, 
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader'
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', {targets: 'defaults'}],
+              '@babel/preset-react'
+            ]
+          }
         }
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+        type: 'asset/inline',
       }
     ]
   },
@@ -27,7 +53,9 @@ module.exports = {
   // Setup plugin to use a HTML file for serving bundled js files
   plugins: [
     new HtmlWebpackPlugin({
-      template: './public/index.html'
+      template: './public/index.html',
+      favicon: './public/favicon.ico',
+      manifest: './public/manifest.json'
     })
   ]
 }
